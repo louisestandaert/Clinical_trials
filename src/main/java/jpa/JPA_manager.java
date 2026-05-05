c4465af79a9e4898d263c748891b7cb2e4706f31
louisestandaert <louise.standaert@usp.ceu.es>
2026-05-05 17:09:24 +0200

    main cambiado

--- src/main/java/jpa/JPA_manager.java
+++ src/main/java/jpa/JPA_manager.java
@@ -52,40 +52,9 @@ public class JPA_manager {
 	}
 }
 
-// ahora hacemos el manager
 
-// jpamanager
-// primero el constructor - segundo los metodos
+// TODO : login , create user (dentro estas creando password) , update(change) password , delete user (opcional), un select user where ... nos inventamos. 
 
-// el constructor
-// el nombre que has puesto en el persistence unit. (xml)
-// un if importante - if my list de roles esta vacía - entonces le añades un
-// role por defecto.
-// tenemos que crear los roles y asignarlos
-
-// ahora los metodos
-//
-
-// empezar con el create role:
-// parametro - un role role
-// em.gettransaction.begin() - esto es para empezar la transaccion
-// em.persist(role) - esto es para persistir el role en la base de datos
-// em.gettransaction.commit() - esto es para confirmar la transaccion
-// y punchhhh!
-
-// para los metodos de usar un query - em.createquery("SELECT r FROM Role r") -
-// esto es para seleccionar todos los roles de la base de datos
-
-// modificar - son transactiones
-// si es solo para ver cosas - query q - q.getresultlist() - esto es para
-// obtener una lista de resultados - haces una query directamente y el segundo
-// parametro , a donde , user.class!
-// q.setparameter("name", name) - esto es para establecer un parametro en la
-// consulta
-// q.getsingleResult() - esto es para obtener un solo resultado
-
-// ahora tines que crear el user - user user = (user) q.getsingleResult() - esto
-// es para obtener un solo resultado y castearlo a user.
 
 // metodo login _ el mas importante
 // tiene que devolverte el user
